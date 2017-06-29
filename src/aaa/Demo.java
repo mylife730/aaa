7a5c0abe3dbf33a6d3351c9d6c06698cd4ab2f44
mylife730 <mylife730@163.com>
2017-06-30 00:52:09 +0800

    爱迪生

--- src/aaa/Demo.java
+++ src/aaa/Demo.java
@@ -5,7 +5,7 @@ public class Demo {
 		System.out.println(333);
 		System.out.println(444);
 		System.out.println(1);
-		System.out.println("出a");
+		System.out.println("出a吧");
 		System.out.println(2);
 	}
 
