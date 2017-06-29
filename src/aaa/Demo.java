ffc96a0d6c31abe2f073559cbb93b3cfdf857014
mylife730 <mylife730@163.com>
2017-06-30 00:43:18 +0800

    大帝

--- src/aaa/Demo.java
+++ src/aaa/Demo.java
@@ -5,7 +5,7 @@ public class Demo {
 		System.out.println(333);
 		System.out.println(444);
 		System.out.println(1);
-		System.out.println("a");
+		System.out.println("出a");
 		System.out.println(2);
 	}
 
